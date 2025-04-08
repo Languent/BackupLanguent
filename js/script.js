51738903f5421d49e91a70aec805ef6c3c03d0a4
Languent <languentltda@gmail.com>
2025-04-08 10:10:56 -0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
-
-const checkbox = document.getElementById("checkbox");
-checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("dark-mode")
-})
