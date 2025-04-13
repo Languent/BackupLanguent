3441b93e9c640a1fe72bd4e75e1ff39db9209048
Languent <languentltda@gmail.com>
2025-04-13 10:58:30 -0300

    Update get_sessao.php

--- php/get_sessao.php
+++ php/get_sessao.php
@@ -2,7 +2,7 @@
 session_start();
 echo json_encode([
     'id_usuario' => $_SESSION['id_usuario'],
-    'id_lingua' => $_SESSION['id_lingua'] ?? null,
+    'id_lingua' => $_SESSION['id_lingua'],
     'nome'=> $_SESSION['nome']
 ]);
 ?>
