6e446107ffd9d6ed56f1d6c49800f2573d06ea73
Pedro-Gabriel-Nobre-da-Mota <pedronobremota@gmail.com>
2025-04-21 22:48:16 -0300

    atualizacao conexion.php

--- php/conection.php
+++ php/conection.php
@@ -6,7 +6,7 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "if0_37044542_languent";
-$dbport = 3307;
+$dbport = 3306;
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport);
 
